18f8edb7a39978dfe8f8c166c5ec9d11a4b5496d
olafro <olka.be@gmail.com>
2020-09-03 15:55:10 +0200

    start

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-var target = 
+var cells = document.getElementsByTagName("td");
+var counter = 0;
+for (item of cells) {
+  item.setAttribute("id", `cellId_${counter}`);
+  item.addEventListener("click", (counter) => {
+    var idCounter = counter.srcElement.id;
+    document.getElementById(idCounter).classList.toggle = "black";
+    console.log(counter.srcElement.id);
+  });
+  counter++;
+}
